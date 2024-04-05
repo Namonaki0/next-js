634974e50580f046849532d4893f7c439aad8a05
Andre <andreferreira@live.co.uk>
2024-04-05 22:57:34 +0100

    route-group-layout

--- src/app/(auth)/forgot-password/page.tsx
+++ src/app/(auth)/forgot-password/page.tsx
@@ -1,3 +1,3 @@
 export default function ForgotPassword() {
-    return <h1 className="fixed inset-0 grid w-full h-full place-content-center bg-black text-white">Forgot password page</h1>
+    return <h1>Forgot password page</h1>
 }
